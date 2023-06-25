4b90e0a3016b53a3ffeef03752cc4c4e4d7abb6b
buiquangkhaiinfo <122881045+buiquangkhaiinfo@users.noreply.github.com>
2023-06-25 18:36:05 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "05052008567890 ",
+            owner: "BUI QUANG KHAI"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0352876128",
+            owner: "Bùi Quang Khải",
+            fastTransfer: true
+        },
+       
+    ]
+};
